b6bbfe96d7897d2b8922c0420925301014dc339a
bolongxi <wilsonwang201206@gmail.com>
2021-02-19 17:03:37 -0500

    change to php

---
+++ AboutUs.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>GQZ TRAVELS</title>
+    <link rel="stylesheet" href="Main.css">
+<body>
+	<?php include('header.php'); ?>
+    <div id="outer">
+		<div id="inner">
+		<h1 class="head">About Us</h1>
+		<div class="head">
+			<img src="" alt="logo">
+		</div>
+		<div class="head">
+			<h3>Why we make this web</h3>
+			<p>Considering the traffic as a serious threat to the quality of life these years, the world has been looking for various
+				solutions to decrease the stress, frustration, delays and terrible air pollutions being caused through it.
+			</p>
+			<h3>We goal</h3>
+			<p>provide a smart green solution on this regard by matching up drivers who live, work, and finally drive in the same
+				neighborhood and would like to provide trip services.</p>
+
+		</div>
+		
+    </div>
+  </div>
+
+</body>
+</html>
