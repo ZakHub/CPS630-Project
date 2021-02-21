bcee9d0cb6e0914c8afc0c1f158cf9a490f1a0ab
bolongxi <wilsonwang201206@gmail.com>
2021-02-20 21:43:41 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require("connect.php");
+if(isset($_POST['login'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $sql = "SELECT UserID, Username, Password FROM Users WHERE Username='$username' AND Password='$password'";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+
+    if ($result->num_rows > 0){
+        // set session variables & redirect to homepage
+        $_SESSION['username'] = $username;
+        $_SESSION['id'] = $row['UserID'];
+        header("Location: CPS630ProjectMain.html");
+    }
+
+    //login failed, redirecting to retry page
+    else{
+        header("Location:Login.html");
+    }
+}
+
