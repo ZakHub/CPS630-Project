3793a576ff329e41a496612222814fe35115364b
bolongxi <wilsonwang201206@gmail.com>
2021-02-20 21:55:54 -0500

    add login

--- navbar.php
+++ navbar.php
@@ -4,6 +4,7 @@
     class="d-inline-block align-top" alt="logo"/>
   <a href="CPS630ProjectMain.php">Home</a>
   <a href="SignUp.html">Sign Up</a>
+  <a href="login.html">Log In</a>
   <a href="AboutUs.php">About Us</a>
   <a href="ContactUs.html">Contact Us</a>
   <a href="Review.html" >Reviews</a>
