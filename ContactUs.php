a84a742a44494e7620ed144e1ca689289e000093
bolongxi <wilsonwang201206@gmail.com>
2021-02-20 20:39:17 -0500

    create contactUs php

---
+++ ContactUs.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>GQZ TRAVELS</title>
+    <link rel="stylesheet" href="Main.css">
+</head>
+<body>
+<div id="outer">
+    <div id="inner">
+        <!-- Navigation -->
+        <?php include('navbar.php') ?>
+        <div class="head">
+            <img src="G.png" alt="logo" width="100%">
+        </div>
+        <h1>Contact Us</h1>
+        <h2>CPS630 GROUP 13</h2>
+        <ul>
+            <li><a href="mailto:qingli.wang@ryerson.ca">Email to QINGLIWANG</a> </li><br>
+            <li><a href="mailto:zabousid@ryerson.ca">Email to Zakaria</a> </li><br>
+            <li><a href="mailto:gregory.norton@ryerson.ca">Email to Gregory</a> </li>
+        </ul>
+
+    </div>
+</div>
+</body>
+</html>
