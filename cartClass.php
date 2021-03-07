8d1665033616725bb8c955a28addb2473df15dee
ZakHub <48577122+ZakHub@users.noreply.github.com>
2021-03-06 22:45:14 -0500

    Add files via upload

---
+++ cartClass.php
@@ -0,0 +1,35 @@
+<?php
+include 'connect.php';
+
+class Cart
+{
+    protected $cartID;
+    protected $trips= [];
+    protected $products=[];
+    protected $totalPrice;
+
+    function __construct($choices = [])
+    {
+    
+    } 
+
+    function gettrips(){}
+
+    function getproduct(){}
+
+    function isEmpty(){}
+
+    function getTotalPrice(){}
+
+    function getTotaltrips(){}
+
+    function getTotalProducts(){}
+
+
+
+
+
+
+}
+
+?>
