fa23d3f4dc4aaf552c941e7de9418f904200c687
Gregory Norton <gregory.norton@me.com>
2021-04-04 18:47:37 -0400

    Added cart totals and removal from cart

--- controllers/cart.js
+++ controllers/cart.js
@@ -5,4 +5,17 @@ function cartController($scope, $window)
 	}
 	
 	$scope.cart = JSON.parse($window.sessionStorage['cart']);
+	
+	$scope.delete = function (which, item) {
+		const i = $scope.cart[which].indexOf(item);
+		if (i > -1) {
+			$scope.cart[which].splice(i, 1);
+			$window.sessionStorage['cart'] = JSON.stringify($scope.cart);
+		}
+	};
+	
+	$scope.total = function () {
+		return $scope.cart.products.map(p => p.price).reduce((a, b) => a + b, 0)
+			+ $scope.cart.trips.map(t => t.price).reduce((a, b) => a + b, 0);
+	};
 }
